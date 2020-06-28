c28c7ebec2db185ffbe4d320cb61190c2ee6e3fb
N04h <contact@n04h.jp>
2020-06-29 00:37:56 +0900

    ✨ ネットの画像表示させる部分まで実装してみた

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Text('Hello World'),
-  ));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("appbar :3"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://wallpaperboys.com/image/2019/12/1125_2436_re_zero_kara_hajimeru_isekai_seikatsu_69.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
