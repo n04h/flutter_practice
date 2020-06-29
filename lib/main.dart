db7b85f4e8530c34c3fdb22b238852ecdc670890
N04h <contact@n04h.jp>
2020-06-30 00:56:55 +0900

    ✨ CardとPaddingをつかって丸みを帯びさせた

--- lib/main.dart
+++ lib/main.dart
@@ -38,48 +38,52 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Container(
+            Card(
               color: Colors.white,
               margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(width: 15),
-                  Text(
-                    '+81 090 1234 5678',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
                     ),
-                  )
-                ],
+                    SizedBox(width: 15),
+                    Text(
+                      '+81 090 1234 5678',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-            Container(
+            Card(
               color: Colors.white,
               margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  SizedBox(width: 15),
-                  Text(
-                    'contact@n04h.jp',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
                     ),
-                  )
-                ],
+                    SizedBox(width: 15),
+                    Text(
+                      'contact@n04h.jp',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
+                ),
               ),
             )
           ],
