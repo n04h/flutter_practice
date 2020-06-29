c0f096cfe76427678432885f7c9cb0b4ece67c3e
N04h <contact@n04h.jp>
2020-06-29 23:59:08 +0900

    ✨ 丸いアイコンを用意した

--- lib/main.dart
+++ lib/main.dart
@@ -13,39 +13,14 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch, // 画面いっぱいにのばすやつ
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Container(
-                    height: 50.0,
-                    child: Text("Container1"),
-                    color: Colors.amber,
-                  ),
-                  Container(
-                    height: 20.0,
-                    child: Text("Container1"),
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 100,
-                color: Colors.blue,
-                child: Text('Container 2'),
-              ),
-              Container(
-                height: 100,
-                color: Colors.red,
-                child: Text('Container 3'),
-              ),
-            ],
-          ),
-        ),
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/icon.jpg'),
+            ),
+          ],
+        )),
       ),
     );
   }
