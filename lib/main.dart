9b2d489f427a046f17bdbadcb77ca0e8b7400bc6
N04h <contact@n04h.jp>
2020-06-30 01:35:23 +0900

    🎨 SizedBoxにDividerをいれて表示させた

--- lib/main.dart
+++ lib/main.dart
@@ -13,69 +13,78 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('images/icon.jpg'),
-            ),
-            Text(
-              'N04h',
-              style: TextStyle(
-                fontSize: 40,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/icon.jpg'),
+              ),
+              Text(
+                'N04h',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'WEB & APP DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.teal.shade100,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 2.5, // 文字の間隔
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
                 color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Pacifico',
-              ),
-            ),
-            Text(
-              'WEB & APP DEVELOPER',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.teal.shade100,
-                fontFamily: 'SourceSansPro',
-                letterSpacing: 2.5, // 文字の間隔
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-              child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.teal,
-                ),
-                title: Text(
-                  '+81 090 1234 5678',
-                  style: TextStyle(
-                    color: Colors.teal.shade900,
-                    fontFamily: 'SourceSansPro',
-                    fontSize: 20,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+81 090 1234 5678',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-              child: ListTile(
-                leading: Icon(
-                  Icons.email,
-                  color: Colors.teal,
-                ),
-                title: Text(
-                  'contact@n04h.jp',
-                  style: TextStyle(
-                    color: Colors.teal.shade900,
-                    fontFamily: 'SourceSansPro',
-                    fontSize: 20,
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'contact@n04h.jp',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20,
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
-        )),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
