e20e7675053523b432b721a5076805f27762720c
N04h <contact@n04h.jp>
2020-06-29 22:11:32 +0900

    ♻️ セクション6用にコードを整理した

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,8 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("appbar :3"),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Image(
-            image: AssetImage('images/diamond.png'),
-          ),
-        ),
+        backgroundColor: Colors.teal,
+        body: Container(),
       ),
     ),
   );
