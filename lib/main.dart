e7a060e899f6df1d4e0b430679e1fd00e2f7bb63
N04h <contact@n04h.jp>
2020-07-06 00:02:52 +0900

    Revert "✨ パッケージを入れてつかってみた"
    This reverts commit ae1529f731e9a2375c43c77db406112dc7d7c8ae.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -9,7 +8,7 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Text(nouns.first),
+          child: Container(),
         ),
       ),
     );
