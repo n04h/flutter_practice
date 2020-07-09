434be1c894a1ad27527cc48e6d0d688aacc10330
N04h <contact@n04h.jp>
2020-07-10 02:36:02 +0900

    ✨ 性別のUIをつくった

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -24,30 +25,42 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     cardColor: activeCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: "MALE",
+                    ),
                   ),
                 ),
                 Expanded(
-                    child: ReusableCard(
-                  cardColor: activeCardColor,
-                )),
+                  child: ReusableCard(
+                    cardColor: activeCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: "FEMALE",
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
           Expanded(
-              child: ReusableCard(
-            cardColor: activeCardColor,
-          )),
+            child: ReusableCard(
+              cardColor: activeCardColor,
+            ),
+          ),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                    child: ReusableCard(
-                  cardColor: activeCardColor,
-                )),
+                  child: ReusableCard(
+                    cardColor: activeCardColor,
+                  ),
+                ),
                 Expanded(
-                    child: ReusableCard(
-                  cardColor: activeCardColor,
-                )),
+                  child: ReusableCard(
+                    cardColor: activeCardColor,
+                  ),
+                ),
               ],
             ),
           ),
@@ -63,12 +76,41 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.cardColor});
+class IconContent extends StatelessWidget {
+  IconContent({this.icon, this.label});
 
-  // const も不変にできるが、コンパイル時に値が計算されたものに固定されるため、
-  // 今回のような場合はfinalを使う必要がある
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          'MALE',
+          style: TextStyle(
+            fontSize: 18,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.cardColor, this.cardChild});
+
   final Color cardColor;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
@@ -78,6 +120,7 @@ class ReusableCard extends StatelessWidget {
         color: cardColor,
         borderRadius: BorderRadius.circular(10.0),
       ),
+      child: cardChild,
     );
   }
 }
