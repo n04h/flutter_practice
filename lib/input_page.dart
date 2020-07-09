f8d1166072e8f44b59ef145256b4ae81ae588459
N04h <contact@n04h.jp>
2020-07-10 01:07:13 +0900

    ✨ Keyに関しては基本使わないので現状は消しておく

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -20,48 +20,16 @@ class _InputPageState extends State<InputPage> {
                   Expanded(
                     child: ReusableCard(),
                   ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ),
+                  Expanded(child: ReusableCard()),
                 ],
               ),
             ),
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-            ),
+            Expanded(child: ReusableCard()),
             Expanded(
               child: Row(
                 children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ),
+                  Expanded(child: ReusableCard()),
+                  Expanded(child: ReusableCard()),
                 ],
               ),
             ),
@@ -71,10 +39,6 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    Key key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Container(
