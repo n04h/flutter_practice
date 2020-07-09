98f89ac5ada6304b614ae9cee78b65ee55656053
N04h <contact@n04h.jp>
2020-07-10 01:13:59 +0900

    ✨ 色を指定できるようにした

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -18,18 +18,32 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: ReusableCard(),
+                    child: ReusableCard(
+                      cardColor: Color(0xFF1D1E33),
+                    ),
                   ),
-                  Expanded(child: ReusableCard()),
+                  Expanded(
+                      child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  )),
                 ],
               ),
             ),
-            Expanded(child: ReusableCard()),
+            Expanded(
+                child: ReusableCard(
+              cardColor: Color(0xFF1D1E33),
+            )),
             Expanded(
               child: Row(
                 children: <Widget>[
-                  Expanded(child: ReusableCard()),
-                  Expanded(child: ReusableCard()),
+                  Expanded(
+                      child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  )),
+                  Expanded(
+                      child: ReusableCard(
+                    cardColor: Color(0xFF1D1E33),
+                  )),
                 ],
               ),
             ),
@@ -39,12 +53,16 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
+  ReusableCard({this.cardColor});
+
+  Color cardColor;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: cardColor,
         borderRadius: BorderRadius.circular(10.0),
       ),
     );
