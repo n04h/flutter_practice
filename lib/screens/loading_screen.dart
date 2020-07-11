3332e84e3810321b548b85617b59737fbda4051f
N04h <contact@n04h.jp>
2020-07-11 18:14:40 +0900

    ✨ APIを叩けるようにした

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -19,11 +20,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     print(location.latitude);
     print(location.longitude);
-      print(e);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
