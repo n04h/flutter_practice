7a50bbb9175264a941a8fd3b9a0c438abecacb95
N04h <contact@n04h.jp>
2020-07-11 18:47:32 +0900

    ✨ jsonDecodeしてデータを取り出してみた

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
@@ -27,7 +29,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
 
     if (response.statusCode == 200) {
-      print(response.body);
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+
+      double temp = decodedData['main']['temp'];
+      String cityName = decodedData['name'];
+
+      print(temp);
+      print(cityName);
     } else {
       print(response.statusCode);
     }
