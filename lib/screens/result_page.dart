14ceeec8fb567be969cb0098776ec5e9d0acaa03
N04h <contact@n04h.jp>
2020-07-11 12:14:36 +0900

    ✨ ディレクトリ分けした

--- lib/screens/result_page.dart
+++ lib/screens/result_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'reusable_card.dart';
-import 'constants.dart';
+import '../components/reusable_card.dart';
+import '../constants.dart';
 
 class ResultPage extends StatelessWidget {
   @override
