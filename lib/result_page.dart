ecb1a3bf0b0bfa225223f6d4f54e857ce0c15c72
N04h <contact@n04h.jp>
2020-07-11 12:06:54 +0900

    ✨ 結果画面のUIをある程度実装した

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'reusable_card.dart';
+import 'constants.dart';
 
 class ResultPage extends StatelessWidget {
   @override
@@ -7,7 +9,61 @@ class ResultPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Text('Hello'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              'Your Result',
+              style: kTitleTextStyle,
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              cardColor: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      'OVERWEIGHT',
+                      style: kResultTextStyle,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      '26.7',
+                      style: kBMITextStyle,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'You have a higher than normal body weight. Try to exercise more.',
+                      style: kBodyTextStyle,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            color: kBottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            padding: EdgeInsets.only(bottom: 20.0),
+            width: double.infinity,
+            height: kBottomContainerHeight,
+            child: Center(
+                child: Text(
+              'RE-CALCULATE',
+              style: kLargeButtonTextStyle,
+            )),
+          )
+        ],
+      ),
     );
   }
 }
