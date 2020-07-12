8658a226a42cee0def37d22a5f77b68d234646c5
N04h <contact@n04h.jp>
2020-07-13 02:01:35 +0900

    ✨ 直接tasksに追加できないようにした

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,20 +1,25 @@
+import 'dart:collection';
 import 'package:flutter/cupertino.dart';
 import 'package:todoey/models/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Buy milk'),
     Task(name: 'Buy eggs'),
     Task(name: 'Buy bread'),
   ];
 
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
+  }
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
   }
 
   void addTask(String newTaskTitle) {
     final task = Task(name: newTaskTitle);
-    tasks.add(task);
+    _tasks.add(task);
     notifyListeners();
   }
 }
