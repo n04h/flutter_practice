c578333603c8b784218fc2281c70c6b27546400d
N04h <contact@n04h.jp>
2020-07-13 01:42:35 +0900

    ✨ Consumerを使うことで反復的な記述を省略できる

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -6,19 +6,23 @@ import 'package:provider/provider.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-          taskTitle: Provider.of<TaskData>(context).tasks[index].name,
-          isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-          checkboxCallback: (value) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              taskTitle: taskData.tasks[index].name,
+              isChecked: taskData.tasks[index].isDone,
+              checkboxCallback: (value) {
 //            setState(() {
 //              Provider.of<TaskData>(context).tasks[index].toggleDone();
 //            });
+              },
+            );
           },
+          itemCount: taskData.tasks.length,
         );
       },
-      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }
