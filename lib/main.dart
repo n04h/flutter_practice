325e12c4425cfbd1b60334680a4b8c32cb80ae7a
N04h <contact@n04h.jp>
2020-07-12 11:23:51 +0900

    ♻️ セクション16用にファイル整理

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:clima/screens/loading_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: LoadingScreen(),
-    );
+    return Container();
   }
 }
