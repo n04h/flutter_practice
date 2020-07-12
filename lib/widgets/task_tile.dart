da7621613be558a1f72bcfc1e7a75dcaa9e0d2fc
N04h <contact@n04h.jp>
2020-07-12 23:24:14 +0900

    🚧 リファクタリング中

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,46 +1,31 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatefulWidget {
-  @override
-  _TaskTileState createState() => _TaskTileState();
-}
+class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
 
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked = false;
+  TaskTile({this.isChecked, this.taskTitle});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        'aa',
+        taskTitle,
         style: TextStyle(
           decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
       ),
-      trailing: TaskCheckbox(
-        checkBoxState: isChecked,
-        checkboxCallback: (bool value) {
-          setState(() {
-            isChecked = value;
-          });
-        },
+      trailing: Checkbox(
+        value: isChecked,
+        activeColor: Colors.lightBlueAccent,
+//        onChanged: checkboxCallback,
       ),
     );
   }
 }
 
-class TaskCheckbox extends StatelessWidget {
-  TaskCheckbox({this.checkBoxState, this.checkboxCallback});
-
-  final bool checkBoxState;
-  final Function checkboxCallback;
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      value: checkBoxState,
-      activeColor: Colors.lightBlueAccent,
-      onChanged: checkboxCallback,
-    );
-  }
-}
+//checkboxCallback: (bool value) {
+//setState(() {
+//isChecked = value;
+//});
+//},
